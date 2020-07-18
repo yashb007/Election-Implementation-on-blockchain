fac610224d1e365246d0f32bba71e7ae1b32569d
yashb007 <bansaly37@gmail.com>
2020-07-18 13:10:01 +0530

    most thing done

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,8 +1,26 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.4.21 <0.7.0;
 
 contract Election {
-string public candidate;
-constructor() public {
-        candidate = "Candidate 1";
-    }
+
+struct Candidate {
+    uint id;
+    string name;
+    uint voteCount;
+}
+
+mapping(uint => Candidate ) public candidates;
+
+uint public candidatesCount;
+
+constructor () public  {
+    addCandidate('candidate 1');
+    addCandidate('candidate 2');
+}
+
+function addCandidate(string memory _name ) private{
+            candidatesCount++;
+            candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+}
+
+
 }
